66c7484dbaf5113dbb5645303a93027bf303be29
Maxime <maxime.gras@etu.univ-lyon1.fr>
2021-02-24 10:21:48 +0100

    début mise en place de la base de données

--- ProjectBDMobile/ViewController.swift
+++ ProjectBDMobile/ViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by lpiem on 24/02/2021.
 //
-
+import CoreData
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    //récupération du contexte
+    private var managedContext: NSManagedObjectContext {
+        (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    private func saveContext() {
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    }
 }
 
